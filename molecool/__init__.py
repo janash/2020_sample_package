784b30cb3747e2ae0eb5492d53d58cb2eb46ecb3
Jessica Nash <janash@vt.edu>
2020-12-09 17:16:19 -0500

    add code and separate into modules

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -8,7 +8,14 @@ from .functions import *
 
 # Handle versioneer
 from ._version import get_versions
+# Add imports here
+from .functions import *
+from .measure import calculate_distance, calculate_angle
+from .molecule import build_bond_list
+from .visualize import draw_molecule, bond_histogram
+from .io import *
+
 versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
 del get_versions, versions
